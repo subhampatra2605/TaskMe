b14be38a19320e26e4a833804e2d55e79581d8dd
subhampatra2605 <154800938+subhampatra2605@users.noreply.github.com>
2026-05-04 21:48:35 +0530

    Update BASE_URL to include '/api' endpoint

--- frontend/src/utils/axioInstance.js
+++ frontend/src/utils/axioInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://taskme-vyn9.onrender.com";
+const BASE_URL = "https://taskme-vyn9.onrender.com/api";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
